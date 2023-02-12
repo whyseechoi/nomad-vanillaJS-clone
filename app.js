65872d5a119772cd4081070f06b92fa3ea912372
YC <44154796+whyseechoi@users.noreply.github.com>
2023-02-12 22:30:46 +0900

    deleted loginForm

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
   console.log("Hi " + loginInput.value);
