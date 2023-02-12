fdd5bb94ad981ffda6da3477502d127de78f3d67
YC <44154796+whyseechoi@users.noreply.github.com>
2023-02-13 00:03:21 +0900

    4.2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const loginInput = document.querySelector("#login-form input");
 /**  submit이 되면 실행되는 함수 */
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(event);
+  console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
