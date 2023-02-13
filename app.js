a1bf56279f7b6a0cf682c93cf78e0926203b1f4e
YC <44154796+whyseechoi@users.noreply.github.com>
2023-02-14 00:17:25 +0900

    4.3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 /**  submit이 되면 실행되는 함수 */
 function onLoginSubmit(event) {
@@ -7,4 +8,10 @@ function onLoginSubmit(event) {
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
